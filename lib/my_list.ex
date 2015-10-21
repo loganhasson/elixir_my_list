143a5c95fa7febc8e7e2eac0b799e9124e4288fb
Logan Hasson <logan.hasson@gmail.com>
2015-10-20 20:47:42 -0400

    Add sample.txt filtering challenge

--- lib/my_list.ex
+++ lib/my_list.ex
@@ -34,3 +34,20 @@ defmodule MyList do
     reverse(t, acc)
   end
 end
+
+defmodule MyFile do
+  def filter_away(file) do
+    case File.read(file) do
+      {:ok, text} ->
+        text
+        |> String.split("\n")
+        |> Stream.filter(&String.starts_with?(&1, "d"))
+        |> Stream.take_every(2)
+        |> Stream.map(&String.capitalize(&1))
+        |> Enum.sort(&(String.length(&1) > String.length(&2)))
+      {:error} ->
+        :error
+    end
+
+  end
+end
