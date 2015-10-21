3c1060f98f2ac34cd62819e97e28b3b61ecea117
Logan Hasson <logan.hasson@gmail.com>
2015-10-21 08:21:12 -0400

    Testing sigils

--- lib/my_list.ex
+++ lib/my_list.ex
@@ -62,3 +62,21 @@ defmodule MyComprehension do
     for item <- list, into: "", do: "/#{item}"
   end
 end
+
+defmodule MySigils do
+  import Kernel, except: [sigil_s: 2]
+
+  def sigil_x(content, _opts) do
+    content
+    |> String.split(" ")
+    |> Enum.map(&("BOO#{&1}EEK"))
+  end
+
+  def sigil_s(content, opts) do
+    content
+  end
+
+  def test_sigil_s(content) do
+    ~s(content)
+  end
+end
