4b9c8945b5d152352bf6be025b691e90002d6335
Logan Hasson <logan.hasson@gmail.com>
2015-10-23 00:09:24 -0400

    Add doctests for MyFilter and fix function

--- lib/learn_elixir/my_filter.ex
+++ lib/learn_elixir/my_filter.ex
@@ -1,4 +1,19 @@
 defmodule LearnElixir.MyFilter do
+  @moduledoc """
+  A function that filters a sample file based on an exercise from LearnElixir.tv
+  """
+
+  @doc """
+  Returns every other word that starts with a 'd', sorted by length
+
+  ## Examples
+
+      iex> LearnElixir.MyFilter.filter_away("sample.txt")
+      ["Data base", "Diamond", "Drill", "Drum"]
+
+      iex> LearnElixir.MyFilter.filter_away("fake_file.txt")
+      :error
+  """
   def filter_away(file) do
     case File.read(file) do
       {:ok, text} ->
@@ -8,7 +23,7 @@ defmodule LearnElixir.MyFilter do
         |> Stream.take_every(2)
         |> Stream.map(&String.capitalize(&1))
         |> Enum.sort(&(String.length(&1) > String.length(&2)))
-      {:error} ->
+      {:error, _} ->
         :error
     end
   end
